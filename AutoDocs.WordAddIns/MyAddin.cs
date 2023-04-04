d0b75fbf32178141d87594113a25ff32f6ed3339
sandip.kharat <sandip.kharat@datafortune.com>
2023-04-04 20:52:35 +0530

    Add project files.

---
+++ AutoDocs.WordAddIns/MyAddin.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.Office.Core;
+using NetOffice.Tools;
+using NetOffice.WordApi;
+using NetOffice.WordApi.Tools;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using NetOffice.OfficeApi;
+using Office = NetOffice.OfficeApi;
+using System.Windows.Forms;
+using ICTPFactory = NetOffice.OfficeApi.ICTPFactory;
+using _CustomTaskPane = NetOffice.OfficeApi._CustomTaskPane;
+using NetOffice.OfficeApi.Tools;
+using Word = NetOffice.WordApi;
+using Microsoft.Win32;
+using NetOffice.VBIDEApi;
+using System.Reflection;
+
+namespace AutoDocs.WordAddIns
+{
+    [ComVisible(true)]
+    [Guid("f7407235-8887-462b-94be-e916fd95b9b9")]
+    [ProgId("AutoDocs.WordAddIns.MyAddin")]
+    [COMAddin("MyAddin", "Addin description.", LoadBehavior.LoadAtStartup)]
+    //[CustomPane(typeof(SampleControl), "AutoDocs WordAddIns", true, PaneDockPosition.msoCTPDockPositionTop, PaneDockPositionRestrict.msoCTPDockPositionRestrictNoVertical, 60, 60)]
+    public class MyAddin : Word.Tools.COMAddin, IDisposable, ICustomTaskPaneConsumer
+    {
+        private static SampleControl _sampleControl;
+        private bool _disposed = false;
+        private static readonly string _prodId = "AutoDocs.TaskPaneAddin";
+
+        private static Word.Application _wordApplication;
+        internal static Word.Application Application { get { return _wordApplication; } }
+        public MyAddin()
+        {
+            this.OnConnection += MyAddin_OnConnection;
+        }
+
+        private void MyAddin_OnConnection(object application, ext_ConnectMode connectMode, object addInInst, ref Array custom)
+        {
+            _wordApplication.DocumentOpenEvent += Application_DocumentOpenEvent;
+        }
+
+        private void Application_DocumentOpenEvent(Document doc)
+        {
+            using (doc)
+            {
+                // start working with the document
+
+            }
+        }
+
+        public void Dispose() => Dispose(true);
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposed)
+            {
+                return;
+            }
+
+            if (disposing)
+            {
+                // Dispose managed state (managed objects).
+                //_ctpFactory?.Dispose();
+            }
+
+            _disposed = true;
+        }
+
+        public void CTPFactoryAvailable(Microsoft.Office.Core.ICTPFactory CTPFactoryInst)
+        {
+            try
+            {
+                Office.ICTPFactory ctpFactory = new NetOffice.OfficeApi.ICTPFactory(_wordApplication, CTPFactoryInst);
+                Office._CustomTaskPane taskPane = ctpFactory.CreateCTP(typeof(MyAddin).Assembly.GetName().Name + ".SampleControl", "AutoDocs TaskPane", Type.Missing);
+                taskPane.DockPosition = (Office.Enums.MsoCTPDockPosition)MsoCTPDockPosition.msoCTPDockPositionRight;
+                taskPane.Width = 300;
+                taskPane.Visible = true;
+                _sampleControl = taskPane.ContentControl as SampleControl;
+                ctpFactory.Dispose();
+            }
+            catch (Exception exception)
+            {
+                string message = string.Format("An error occured.{0}{0}{1}", Environment.NewLine, exception.Message);
+                MessageBox.Show(message, _prodId, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
---
+++ AutoDocs.WordAddIns/SampleControl.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace AutoDocs.WordAddIns
+{
+    partial class SampleControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSaveDocument = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSaveDocument
+            // 
+            this.btnSaveDocument.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSaveDocument.Location = new System.Drawing.Point(23, 15);
+            this.btnSaveDocument.Name = "btnSaveDocument";
+            this.btnSaveDocument.Size = new System.Drawing.Size(250, 23);
+            this.btnSaveDocument.TabIndex = 0;
+            this.btnSaveDocument.Text = "Save Document";
+            this.btnSaveDocument.UseVisualStyleBackColor = true;
+            this.btnSaveDocument.Click += new System.EventHandler(this.btnSaveDocument_Click);
+            // 
+            // SampleControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btnSaveDocument);
+            this.Name = "SampleControl";
+            this.Size = new System.Drawing.Size(300, 607);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSaveDocument;
+    }
+}
